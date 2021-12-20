e169134da819f41513c42fbb78ae702c81d55380
Jagdish8 <jpawar8105@gmail.com>
2021-12-20 23:42:19 +0530

    hard
    hard

--- Leetcode/Recursion/10. Regular Expression Matching.py
+++ Leetcode/Recursion/10. Regular Expression Matching.py
@@ -4,30 +4,28 @@
 # '*' Matches zero or more of the preceding element.
 # The matching should cover the entire input string (not partial).
 
- 
-
 # Example 1:
-
 # Input: s = "aa", p = "a"
 # Output: false
 # Explanation: "a" does not match the entire string "aa".
-# Example 2:
 
+# Example 2:
 # Input: s = "aa", p = "a*"
 # Output: true
 # Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
-# Example 3:
 
+# Example 3:
 # Input: s = "ab", p = ".*"
 # Output: true
-# Explanation: ".*" means "zero or more (*) of any character (.)".
-# Example 4:
+# Explanation: ".*" means "zero or more (*) of any character (.)"
 
+# Example 4:
 # Input: s = "aab", p = "c*a*b"
 # Output: true
 # Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore, it matches "aab".
-# Example 5:
 
+# Example 5:
 # Input: s = "mississippi", p = "mis*is*p*."
 # Output: false
+
 
